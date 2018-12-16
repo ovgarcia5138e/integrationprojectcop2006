64875950c9f71f15e64ceb1ef52eb82d45bf15c7
ovgarcia5138e <36053669+ovgarcia5138e@users.noreply.github.com>
2018-12-15 22:53:30 -0500

    Update ArrayLIst.java

--- ArrayLIst.java
+++ ArrayLIst.java
@@ -20,30 +20,32 @@ public class ArrayLIst {
    * 
    * @OscarGarcia
    */
+  
+  // code sourced from chegg tutor https://www.chegg.com/homework-help/questions-and-answers/java-declare-arraylist-integers-put-first-100-integers-list-write-loop-walks-arraylist-pri-q24125821?trackid=86049f5a907c&strackid=c17fb3e8bfa2&ii=10
 
   public static void main(String[] args) {
 
-    List<Integer> integerList = new ArrayList<Integer>();
+    List<Integer> arrayListInt = new ArrayList<Integer>();
 
     System.out.println("Below we will print an array list you will notice it is not "
         + " an efficent manner to view an array list." + "\n");
 
-    for (int i = 1; i < 101; i++) {
+    for (int i = 1; i < 61; i++) {
 
-      integerList.add(i);
+      arrayListInt.add(i);
 
     }
 
-    System.out.println(integerList);
+    System.out.println(arrayListInt);
 
     System.out.println("OR");
 
     // OR use forEach loop
 
-    for (Integer val : integerList) {
+    for (Integer val : arrayListInt) {
       System.out.print(val + " ");
 
-      // IF condition only for proper alignment only
+      // IF condition will be used for corret  alignment only
 
       if (val % 10 == 0) {
 
@@ -65,12 +67,19 @@ public class ArrayLIst {
    */
 
   public static void main1(String[] args) {
-    locateStar(board);
+    locateStar(layOut);
   }
 
 
 
-  static char[][] board =
+  private static void locateStar(char[][] layOut2) {
+    // TODO Auto-generated method stub
+
+  }
+
+
+
+  static char[][] layOut =
       {{'.', '.', '.', '.'}, {'.', '.', '.', '.'}, {'.', '.', '.', '*'}, {'.', '.', '.', '.'}};
 
   /**
@@ -80,12 +89,12 @@ public class ArrayLIst {
    *
    */
 
-  public static void locateStar(char[][] board) {
+  public static void findLocation(char[][] board) {
 
 
-    for (int i = 0; i < board.length; i++) {
-      for (int j = 0; j < board[0].length; j++) {
-        if (board[i][j] == '*') {
+    for (int i = 0; i < layOut.length; i++) {
+      for (int j = 0; j < layOut[0].length; j++) {
+        if (layOut[i][j] == '*') {
           System.out.println("Found at " + i + " " + j);
           break;
         }
@@ -93,4 +102,3 @@ public class ArrayLIst {
     }
   }
 }
-
