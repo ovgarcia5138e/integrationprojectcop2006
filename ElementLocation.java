13323e016d2e073ee8be6897025f371286e0fcd5
ovgarcia5138e <36053669+ovgarcia5138e@users.noreply.github.com>
2018-12-15 23:05:40 -0500

    Update ElementLocation.java

--- ElementLocation.java
+++ ElementLocation.java
@@ -12,29 +12,34 @@ public class ElementLocation {
    * @OscarGarcia
    */
 
+
+
+  /**
+   * @param args code sourced from
+   *        
+   */
+
   public static void main(String[] args) {
 
-    Random rnd = new Random();
+    Random randomNumber = new Random();
 
-    int[] randomIntegers = new int[20];
+    int[] randomArrayInt = new int[20];
 
-    double smallest = randomIntegers[0];
+    double smallest = randomArrayInt[0];
 
     System.out.println("Below we will print a mulitple randomly generated array"
         + " and run the array through a for loop that will"
         + " return the index number of the smallest valued element. " + "\n");
 
-    for (int i = 0; i < randomIntegers.length; i++) { // fills array with random integers
-      randomIntegers[i] = rnd.nextInt(99) + 1;
-      System.out.println("Idex Number: " + i + " Element: " + randomIntegers[i]);
+    for (int i = 0; i < randomArrayInt.length; i++) { // Produces array with random integers
+      randomArrayInt[i] = randomNumber.nextInt(99) + 1;
+      System.out.println("Idex Number: " + i + " Element: " + randomArrayInt[i]);
     }
 
-    for (int i = 0; i < randomIntegers.length; i++) {
-      // get the location of smallest number in
-      // the
-      // array
-      if (randomIntegers[i] < smallest) {
-        smallest = randomIntegers[i];
+    for (int i = 0; i < randomArrayInt.length; i++) {
+      //Produce the location of smallest number
+      if (randomArrayInt[i] < smallest) {
+        smallest = randomArrayInt[i];
       }
     }
 
